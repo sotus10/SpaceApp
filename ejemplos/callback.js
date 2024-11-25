efc91e9df643cde01c5a27abfb0b16dcd133571c
Juan Diego Soto <hornodelaabue@gmail.com>
2024-11-25 18:28:45 -0500

    Ejemplos de callback

---
+++ ejemplos/callback.js
@@ -0,0 +1,18 @@
+function uno (dos) {
+    console.log("uno")
+    setTimeout(dos, 2000)
+}
+
+function dos () {
+    console.log("dos")
+}
+
+setTimeout(() => uno(dos), 5000)
+
+const nombre = function() {
+    console.log("nombre")
+}
+
+nombre()
+
+const persona = () => console.log("Juan")
