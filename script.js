7a821a66ef147e2814b086692374681c68648984
Juan Diego Soto <hornodelaabue@gmail.com>
2024-11-27 16:55:58 -0500

    Update JS

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5"
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15"
 
-//El fetch API es una función que nos permite usar la API
-fetch(url).
+/*function listaDeImagenes() {
+    //El fetch API es una función que nos permite usar la API
+    fetch(url)
+    //En el then escribimos lo que debe pasar si se cumple la promesa
+    .then( response => response.json())
+    .then( datosImagenes => {
+        //El console.log es para revisar que si se este conectando con la API
+        console.log(datosImagenes)
+        const card = document.querySelector("[data-ul]")
+        //Modificamos el HTML con los datos de la API por medio de JS
+        datosImagenes.forEach(elemento => {
+            const contenido = `<li class="card">
+                        <img class="card__image" src="${elemento.url}" alt="imagen">
+                        <h3 class="card__title">${elemento.title}</h3>
+                    </li>`
+            //Hacemos los cambios en el HTML
+            card.innerHTML = card.innerHTML + contenido
+        });
+    })
+    //El catch es por si la promesa no se cumple, y le hacemos un console.log para ver el error.
+    .catch( error => console.log(error))
+}
+
+//Ponemos a trabajar la función
+listaDeImagenes()*/
